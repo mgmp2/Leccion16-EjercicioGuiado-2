d6c0bfdd86d723d35b2b714c5554ab3bd52ce5e1
Miriam Mendoza Pilco <miriammp1997@gmail.com>
2017-03-04 20:20:52 -0500

    agregando un output

--- js/archivo.js
+++ js/archivo.js
@@ -1,5 +1,5 @@
 function pesoPlaneta(){
-    
+
     var peso = document.getElementById("peso");
     var jupiter = peso.value * 0.39;
     var venus   = peso.value * 0.87;
@@ -7,14 +7,16 @@ function pesoPlaneta(){
     var marte   = peso.value * 0.38;
     var saturno = peso.value * 0.93;
     var neptuno = peso.value * 1.38;
-        
+
+    if(peso.value){
     document.getElementById("resultado").innerHTML = "<strong>"+
                                                       "<ul>" +
-                                                      "<li>" + "Peso en Jupiter: " + jupiter + "</li>" +
-                                                      "<li>" + "Peso en Venus: "+ venus + "</li>" +
-                                                      "<li>" + "Peso en Urano: " + urano + "</li>" +
-                                                      "<li>" + "Peso en Marte: "+marte + "</li>"+
-                                                      "<li>" + "Peso en Saturno: "+saturno + "</li>"+
-                                                      "<li>" + "Peso en Neptuno: "+neptuno + "</li>"+
-                                                      "</strong>";
+                                                      "<li>" + "Peso en Jupiter: " + jupiter.toFixed(2) + "</li>" +
+                                                      "<li>" + "Peso en Venus: "+ venus.toFixed(2) + "</li>" +
+                                                      "<li>" + "Peso en Urano: " + urano.toFixed(2) + "</li>" +
+                                                      "<li>" + "Peso en Marte: "+marte.toFixed(2) + "</li>"+
+                                                      "<li>" + "Peso en Saturno: "+saturno.toFixed(2) + "</li>"+
+                                                      "<li>" + "Peso en Neptuno: "+neptuno.toFixed(2) + "</li>"+
+                                                      "</ul></strong>";}
+
 }
